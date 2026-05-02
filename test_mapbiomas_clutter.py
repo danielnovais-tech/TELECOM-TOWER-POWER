c7c14736dc6b4fb8d5338dcebb4920847868688f
Daniel Azevedo Novais <daniel.novais@sempreceub.com>
2026-05-02 18:54:11 -0300

    chore(license): add SPDX headers to test files for license-scan CI

--- test_mapbiomas_clutter.py
+++ test_mapbiomas_clutter.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LicenseRef-TTP-Proprietary
+# Copyright (c) 2026 Daniel Azevedo Novais ("TELECOM-TOWER-POWER"). All rights reserved.
 """Tests for the MapBiomas clutter extractor.
 
 These tests do NOT require the `rasterio` package or an actual GeoTIFF
--- test_optimize_sites.py
+++ test_optimize_sites.py
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: LicenseRef-TTP-Proprietary
+# Copyright (c) 2026 Daniel Azevedo Novais ("TELECOM-TOWER-POWER"). All rights reserved.
 """Tests for the site-placement GA.
 
 Covers two regressions hit on 2026-05-02:
